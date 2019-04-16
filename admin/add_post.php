cd7360e4f4744140402bb0bcb616e178e0d4c858
Alex Gumbin <lesha.gumbin@yandex.ru>
2019-04-16 10:51:29 +0300

    add_post added

---
+++ admin/add_post.php
@@ -0,0 +1,52 @@
+
+<div class="add-post">
+  <form class="add-post-form" action="admin.php" method="post" enctype="multipart/form-data">
+    <div class="form-container">
+
+      <div class="form-input-div" style="width: 50%">
+        <span>
+          Название:
+        </span>
+        <input type="text" name="post_name" autofocus style="width:200px" class="form-input">
+      </div>
+
+      <div class="form-input-div" style="width: 50%">
+        <span>
+          Рубрика:
+        </span>
+        <select class="select" name="post_section" size="1">
+          <option value="Покатушки" class="select-option">Покатушки</option>
+          <option value="Путешествия" class="select-option">Путешествия</option>
+          <option value="Полезное" class="select-option">Полезное</option>
+          <option value="Интересное" class="select-option">Интересное</option>
+        </select>
+      </div>
+
+      <div class="form-input-div">
+        <span>
+          Заглавная фотография:
+        </span>
+        <input type="file" name="post_title_img" accept="image/*">
+      </div>
+
+      <div class="form-input-div">
+        <span>
+          Краткое описание:
+        </span>
+        <textarea name="post_short" rows="8" cols="80" class="textarea post-short"></textarea>
+      </div>
+
+      <div class="form-input-div">
+        <span>
+          Содержание:
+        </span>
+        <textarea name="post_content" rows="8" cols="80" class="textarea post-content"></textarea>
+      </div>
+
+      <div>
+        <input type="submit" name="submit-button" value="Опубликовать" class="submit-button">
+      </div>
+
+    </div>
+  </form>
+</div>
--- admin/admin.php
+++ admin/admin.php
@@ -1,4 +1,22 @@
 <?php include '../database/connection.php' ?>
+<?php
+// if(isset($_FILES['post_title_img'])){
+//   $errors= array();
+//   $file_name = $_FILES['post_title_img']['name'];
+//   $file_size =$_FILES['post_title_img']['size'];
+//   $file_tmp =$_FILES['post_title_img']['tmp_name'];
+//   $file_type=$_FILES['post_title_img']['type'];
+//   echo $file_name;
+//
+//   if(empty($errors)==true){
+//     move_uploaded_file($file_tmp,"images/".$file_name);
+//     echo "Success";
+//   } else {
+//     print_r($errors);
+//   }
+// }
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,14 +36,45 @@
         <span class="title">Blog about cycling</span>
         <img src="../images/headericon.png" alt="Icon" class="title-icon">
       </div>
-      <div>
+      <div style="margin-bottom: 10px;">
         <span style="font-size: 20px;">Admin</span>
       </div>
+
+      <div class="menu-container">
+        <ul class="menu">
+          <li class="menu-item">
+              <div class="menu-item-link">
+                Добавить запись
+              </div>
+          </li>
+          <li class="menu-item">
+              <div class="menu-item-link">
+                Редактировать записи
+              </div>
+          </li>
+          <li class="menu-item">
+              <div class="menu-item-link">
+                Разместить рекламу
+              </div>
+          </li>
+          <!-- <li class="menu-item">
+              <div class="menu-item-link">
+                Полезное
+              </div>
+          </li>
+          <li class="menu-item">
+              <div class="menu-item-link">
+                Интересное
+              </div>
+          </li> -->
+        </ul>
+      </div>
+
     </div>
   </div>
   <div class="main">
     <div class="container container-main">
-
+      <?php @include 'add_post.php' ?>
     </div>
   </div>
 </body>
